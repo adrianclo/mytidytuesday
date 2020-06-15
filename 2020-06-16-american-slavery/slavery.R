d66762b8c31bf2698aa8703b4129147244aa4f12
adrianclo <adrianclo1984@gmail.com>
2020-06-15 18:02:49 +0200

    Update slavery.R

--- 2020-06-16-american-slavery/slavery.R
+++ 2020-06-16-american-slavery/slavery.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(tidytuesdayR)
 
-tuesdata <- tt_load('2020-06-16')
+tuesdata <- tt_load("2020-06-16")
 tuesdata
 
 census <- tuesdata$census
@@ -12,6 +12,13 @@ blackpast <- tuesdata$blackpast
 View(census)
 View(names)
 View(routes)
+View(blackpast)
 
 routes %>% 
   filter(voyage_id == 2314)
+
+census %>% 
+  count(region, division)
+
+names %>% 
+  count(gender)
